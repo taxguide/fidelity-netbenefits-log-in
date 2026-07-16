49fcc04d65b56cf04c3f33ec331964d7c7b6d934
taxguide <singhparash06@gmail.com>
2026-07-16 14:39:31 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,9 @@ project = "fidelity"
 author = "fidelity"
 release = "1.0"
 
-extensions = []
+extensions = [
+    "sphinx_sitemap",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -10,3 +12,6 @@ exclude_patterns = []
 html_theme = "alabaster"
 
 html_static_path = ["_static"]
+
+html_baseurl = "https://fidelity-fidelity-netbenefits-log-in.readthedocs-hosted.com/"
+sitemap_url_scheme = "{link}"
